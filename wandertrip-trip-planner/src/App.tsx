48c9a8dbb66f1533e9f20462c40e31fb5071fa28
sanketkalekar <kalekarsanket005@gmail.com>
2025-05-21 11:15:53 +0530

    first commit

---
+++ wandertrip-trip-planner/src/App.tsx
@@ -0,0 +1,13 @@
+
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1 className="title">The Mini Trip Planner</h1>
+    </div>
+  )
+}
+
+export default App
